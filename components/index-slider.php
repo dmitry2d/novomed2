0f101b207080233b180a57e9b45a5038117630a8
Dmitry <volcharo@gmail.com>
2024-08-19 15:11:46 +0300

    code

--- components/index-slider.php
+++ components/index-slider.php
@@ -1,55 +1,98 @@
 
-<!-- Слайдер -->
-<div id="carouselExampleControls" class="index-slider carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner">
-    
-        <!-- Слайд -->
-        <div class="carousel-item position-relative active">
-            <img class="position-absolute w-100 h-100" src="./assets/img/slider_bg_1.jpg" style="object-fit: cover" alt="">
-            <div class="position-absolute w-100 h-100 bg-dark" style="opacity: 0.7"></div>
-            <div class="position-relative w-100 text-white">
-                <div class="container-md p-4">
-                    <br><br><br>
-                    <h4>Прием пластического хирурга</h4>
-                    <h1>Абрамова И.В.</h1>
-                    <h3 class="py-4">1000 рублей</h3>
-                    <br><br>
-                    <a href="" class="btn btn-outline-light border-2">Узнать подробно</a>
-                    <br><br><br>
-                </div>
-            </div>
-        </div>
-        <!-- / Слайд -->
-    
-    
-        <!-- Слайд -->
-        <div class="carousel-item position-relative">
-            <img class="position-absolute w-100 h-100" src="./assets/img/slider_bg_1.jpg" style="object-fit: cover" alt="">
-            <div class="position-absolute w-100 h-100 bg-dark" style="opacity: 0.7"></div>
-            <div class="position-relative w-100 text-white">
-                <div class="container-md p-4">
-                    <br><br><br>
-                    <h4>Прием пластического хирурга</h4>
-                    <h1>Абрамова И.В.</h1>
-                    <h3 class="py-4">1000 рублей</h3>
-                    <br><br>
-                    <a href="" class="btn btn-outline-light border-2">Узнать подробно</a>
-                    <br><br><br>
-                </div>
-            </div>
-        </div>
-        <!-- / Слайд -->
-    
 
+
+
+<!-- Слайдер -->
+<div class="container">
+    <div class="index-slider bg-primary my-4">
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+
+                <div class="carousel-item active">
+                    <div class="row h-100 flex-column flex-md-row">
+                        <div class="col-md-6 col-xl-7 photo order-md-2">
+                            <img src="./assets/img/slider/slider_photo_01.png" alt="">
+                        </div>
+                        <div class="col-md-6 col-xl-5 text-white p-0 p-md-5 order-md-1 flex-grow-1">
+                            <div class="d-flex flex-column h-100 p-5">
+                                <div class="fs-3 fs-md-2 fs-lg-1 pb-0 pb-md-4 pb-xl-5">
+                                    Центр хирургической помощи
+                                </div>
+                                <div class="fs-4 flex-grow-1">
+                                    Малоинвазивные операции
+                                </div>
+                                <div class="link pb-5 pb-md-4">
+                                    <a href="/" class="text-center d-block d-md-inline text-white bg-nm-medblue px-5 py-2 py-md-4 text-decoration-none rounded-pill fs-5 fs-md-4">
+                                        Подробнее
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row h-100 flex-column flex-md-row">
+                        <div class="col-md-6 col-xl-7 photo order-md-2">
+                            <img src="./assets/img/slider/slider_photo_02.png" alt="">
+                        </div>
+                        <div class="col-md-6 col-xl-5 text-white p-0 p-md-5 order-md-1 flex-grow-1">
+                            <div class="d-flex flex-column h-100 p-5">
+                                <div class="fs-3 fs-md-2 fs-lg-1 pb-0 pb-md-4 pb-xl-5">
+                                    Лечение зубов во сне и наяву
+                                </div>
+                                <div class="fs-4 flex-grow-1">
+                                    Если проснетесь, то с зубами
+                                </div>
+                                <div class="link pb-5 pb-md-4">
+                                    <a href="/" class="text-center d-block d-md-inline text-white bg-nm-medblue px-5 py-2 py-md-4 text-decoration-none rounded-pill fs-5 fs-md-4">
+                                        Подробнее
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="row h-100 flex-column flex-md-row">
+                        <div class="col-md-6 col-xl-7 photo order-md-2">
+                            <img src="./assets/img/slider/slider_photo_03.png" alt="">
+                        </div>
+                        <div class="col-md-6 col-xl-5 text-white p-0 p-md-5 order-md-1 flex-grow-1">
+                            <div class="d-flex flex-column h-100 p-5">
+                                <div class="fs-3 fs-md-2 fs-lg-1 pb-0 pb-md-4 pb-xl-5">
+                                    Прослушка Ваших органов
+                                </div>
+                                <div class="fs-4 flex-grow-1">
+                                    Думайте о чем нибудь тайном
+                                </div>
+                                <div class="link pb-5 pb-md-4">
+                                    <a href="/" class="text-center d-block d-md-inline text-white bg-nm-medblue px-5 py-2 py-md-4 text-decoration-none rounded-pill fs-5 fs-md-4">
+                                        Подробнее
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     </div>
-    <button class="carousel-control-prev w-auto px-3" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next w-auto px-3" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
 </div>
 
 <!-- Слайдер -->
+
+<br><br><br><br><br><br>
